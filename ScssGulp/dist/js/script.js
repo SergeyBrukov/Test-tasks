50ba9494eae684803d0ac2e9628baf1cffe2e44e
Sergey <sdiomant@gmail.com>
2022-01-15 22:23:48 +0200

    ScssGulp

---
+++ ScssGulp/dist/js/script.js
@@ -0,0 +1,269 @@
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+
+///form///
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		
+		let error = formValidate(form);
+		if (error === 0) {
+		} else {
+			alert('Заповніть обовязкові поля');
+			e.preventDefault();
+		}
+
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains('_email')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+});
+////////////////////
+
+///form-reg///
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	
+	const form = document.getElementById('form__reg');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		
+		let error = formValidate(form);
+		if (error === 0) {
+		} else {
+			alert('Заповніть обовязкові поля');
+			e.preventDefault();
+		}
+
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req-reg');
+
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formRemoveError(input);
+
+			if (input.classList.contains('_email-reg')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			} else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+				}
+			}
+		}
+		return error;
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+});
+////////////////////
+
+
+
+
+
+// Инициализируем Swiper
+let myImageSlider = new Swiper('.image-slider', {
+	// Стрелки
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Управление колесом мыши
+	mousewheel: {
+		// Чувствительность колеса мыши
+		sensitivity: 1,
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 4,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: true,
+
+	// Отступ между слайдами
+	// spaceBetween: 30,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: false,
+
+	// Стартовый слайд.
+	initialSlide: 0,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: true,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: true,
+
+
+	// Скорость
+	speed: 800,
+
+	// Вертикальный слайдер
+	direction: 'horizontal',
+
+
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',
+
+
+	// Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		450: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 1,
+		},
+		850: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		},
+		1600: {
+			slidesPerView: 4,
+		}
+	},
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+});
+
